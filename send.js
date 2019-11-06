98af139707a9395313f70cb3ca0ecd3553046d6e
Julian Henao Marin <>
2019-11-06 16:55:57 -0500

    Other way to connect

--- send.js
+++ send.js
@@ -1,6 +1,6 @@
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(err, conn) {
+amqp.connect({ protocol: 'amqp', hostname: '10.0.0.54', port: 5672, username: 'picizweb', password: 'p1c1zw3b@2019*.*', vhost: 'PRD' }, function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'hello';
     var msg = 'Hello World!';
@@ -9,6 +9,6 @@ amqp.connect('amqp://localhost', function(err, conn) {
     // Note: on Node 6 Buffer.from(msg) should be used
     ch.sendToQueue(q, new Buffer(msg));
     console.log(" [x] Sent %s", msg);
+    setTimeout(function() { conn.close(); process.exit(0) }, 500);
   });
-  setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
